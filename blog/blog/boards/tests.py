2f319f1a248daee4ca89e8e93228b2749b41a5a5
ivan1994petrov <ivan94hristov@gmail.com>
2020-01-30 16:33:03 +0200

    +1 test for home

--- blog/blog/boards/tests.py
+++ blog/blog/boards/tests.py
@@ -7,6 +7,11 @@ from .models import Board
 
 
 class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
@@ -15,6 +20,10 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEqual(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href=:"{}"'.format(board_topics_url))
 
 
 class BoardTopicsTests(TestCase):
