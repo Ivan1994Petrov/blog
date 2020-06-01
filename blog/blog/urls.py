a92ac83c759b2b9cc3ef57366cb836c84225a615
ivan1994petrov <ivan94hristov@gmail.com>
2020-06-01 20:05:57 +0300

    new blog

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,15 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-
-from .accounts import views as accounts_view
-from .boards import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^signup/$', accounts_view.signup, name='signup'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
